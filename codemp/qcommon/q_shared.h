6156cbbc7678257c1eee66127ced602bf726a1d5
jampio <jampio@mail.com>
2018-02-14 08:40:47 -0800

    change unix homepath to jampog

--- codemp/qcommon/q_shared.h
+++ codemp/qcommon/q_shared.h
@@ -31,7 +31,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 #define CLIENT_WINDOW_TITLE "OpenJK (MP)"
 #define CLIENT_CONSOLE_TITLE "OpenJK Console (MP)"
-#define HOMEPATH_NAME_UNIX "openjk"
+#define HOMEPATH_NAME_UNIX "jampog"
 #define HOMEPATH_NAME_WIN "OpenJK"
 #define HOMEPATH_NAME_MACOSX HOMEPATH_NAME_WIN
 
