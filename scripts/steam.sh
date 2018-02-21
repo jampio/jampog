a38f2f6332c998c6e7a32dc4a9b3d30aa8edbf88
jampio <jampio@mail.com>
2018-02-21 15:20:12 +0000

    update build file

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-mkdir -p /home/$USER/.local/share/jampog/base/ &&
-apt install steamcmd &&
+wget http://media.steampowered.com/client/steamcmd_linux.tar.gz -O /tmp/steamcmd.tar.gz &&
+mkdir -p /tmp/steamcmd &&
+tar -xvzf /tmp/steamcmd.tar.gz -C /tmp/steamcmd &&
 read -s -p "Enter Steam Username: " STEAM_USER &&
 echo &&
 read -s -p "Enter Steam Password: " STEAM_PASS &&
 echo &&
-/usr/games/steamcmd \
+/tmp/steamcmd/steamcmd.sh \
 	+@sSteamCmdForcePlatformType windows \
 	+login $STEAM_USER $STEAM_PASS \
 	+force_install_dir /tmp/SteamJKA \
-	 +app_update 6020 validate +quit &&
-mv /tmp/SteamJKA/GameData/base/assets0.pk3 /home/$USER/.local/share/jampog/base/ &&
-mv /tmp/SteamJKA/GameData/base/assets1.pk3 /home/$USER/.local/share/jampog/base/ &&
-mv /tmp/SteamJKA/GameData/base/assets2.pk3 /home/$USER/.local/share/jampog/base/ &&
-mv /tmp/SteamJKA/GameData/base/assets3.pk3 /home/$USER/.local/share/jampog/base/ &&
-rm -rf /tmp/SteamJKA
+	 +app_update 6020 validate +quit
+STEAM_PATH=/tmp/SteamJKA/GameData/base
+INSTALL_PATH=/home/$USER/.local/share/jampog/base/
+mkdir -p $INSTALL_PATH
+[ -f $STEAM_PATH/assets0.pk3 ] && mv $STEAM_PATH/assets0.pk3 $INSTALL_PATH
+[ -f $STEAM_PATH/assets1.pk3 ] && mv $STEAM_PATH/assets1.pk3 $INSTALL_PATH
+[ -f $STEAM_PATH/assets2.pk3 ] && mv $STEAM_PATH/assets2.pk3 $INSTALL_PATH
+[ -f $STEAM_PATH/assets3.pk3 ] && mv $STEAM_PATH/assets3.pk3 $INSTALL_PATH
